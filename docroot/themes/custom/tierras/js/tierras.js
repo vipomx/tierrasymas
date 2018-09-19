ca850ca45ccf0485e0ba35c750978ef51a9be41f
vipomx <vpolancop@gmail.com>
2018-09-19 11:03:22 -0600

    Global styles and map feature.

--- docroot/themes/custom/tierras/js/tierras.js
+++ docroot/themes/custom/tierras/js/tierras.js
@@ -4,14 +4,36 @@
  *
  */
 (function ($, Drupal) {
+  if ($('#menu-opener').length > 0) {
+    mobileMenu();
+    mapFilters();
+  }
 
-  /**
-   * Use this behavior as a template for custom Javascript.
-   */
-  Drupal.behaviors.exampleBehavior = {
-    attach: function (context, settings) {
-      //alert("I'm alive!");
+  // Manages mobile menu.
+  function mobileMenu() {
+    $('#menu-opener').click(function(event) {
+      event.preventDefault();
+      if ($('#block-tierras-main-menu').length > 0) {
+        if ($('#block-tierras-main-menu').hasClass('open')) {
+          $('#block-tierras-main-menu').removeClass('open');
+          $('#menu-opener').removeClass('to-close');
+        }
+        else {
+          $('#block-tierras-main-menu').addClass('open');
+          $('#menu-opener').addClass('to-close');
+        }
+      }
+    });
+  }
+
+  // Manages map filters.
+  function mapFilters() {
+    if ($('#map-filters').length > 0) {
+      $('#map-filters').click(function(event) {
+        event.preventDefault();
+        $(this).parent().toggleClass('open');
+      });
     }
-  };
+  }
 
 })(jQuery, Drupal);
